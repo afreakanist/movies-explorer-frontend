369050809531131b65d745dfdd0ce5fca168ad8e
afreakanist <afreakanist@gmail.com>
2021-09-14 18:35:36 +0600

    App component updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,49 @@
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
-//import { useEffect, useState } from "react";
-import Header from "../Header/Header";
-import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Main from "../Main/Main";
+import Movies from "../Movies/Movies";
+import NotFound from "../NotFound/NotFound";
+import Profile from "../Profile/Profile";
+import Register from "../Register/Register";
+import SavedMovies from "../SavedMovies/SavedMovies";
 
 function App() {
   return (
-    <div className="page">
-      <Header isLoggedIn={false} />
-      <main className="content">
-        <Switch>
-          <Route path="/">
-            <Main />
-          </Route>
-        </Switch>
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Header isLoggedIn={false} />
+          <Main />
+          <Footer />
+        </Route>
+        <Route exact path="/signup">
+          <Register />
+        </Route>
+        <Route exact path="/signin">
+          <Login />
+        </Route>
+        <Route exact path="/profile">
+          <Header isLoggedIn={true} />
+          <Profile />
+        </Route>
+        <Route exact path="/movies">
+          <Header isLoggedIn={true} />
+          <Movies />
+          <Footer />
+        </Route>
+        <Route exact path="/saved-movies">
+          <Header isLoggedIn={true} />
+          <SavedMovies />
+          <Footer />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
